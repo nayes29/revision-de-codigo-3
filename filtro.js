e7f408dbfa71c204fe757cb948f5fa4f3ec8b563
Nallely Sifuentes <sifuentesnallely8@gmail.com>
2023-12-12 19:52:47 -0600

    revision de codigo 3

--- filtro.js
+++ filtro.js
@@ -1,63 +1,88 @@
-// Tenemos un li de productos
-
+// Arreglo de objetos que contiene información sobre los productos
 const productos = [
   {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
   {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
   {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
   {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
-]
+];
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+// Obtener el elemento que contiene la lista de productos
+const li = document.getElementById("lista-de-productos");
 
-for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+// Obtener el elemento de entrada de texto
+const $i = document.getElementById('input');
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
-  
-  var imagen = document.createElement("img");
-  imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+// Función que crea la lista de productos
+function displayProductos(productos) {
+  for (let i = 0; i < productos.length; i++) {
+    var d = document.createElement("div");
+    d.classList.add("producto");
 
-  li.appendChild(d)
+    var ti = document.createElement("p");
+    ti.classList.add("titulo");
+    ti.textContent = productos[i].nombre;
+    
+    var imagen = document.createElement("img");
+    imagen.setAttribute('src', productos[i].img);
+
+    d.appendChild(ti);
+    d.appendChild(imagen);
+
+    li.appendChild(d);
+  }
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+// Mostrar la lista de productos
+displayProductos(productos);
 
+
+// Obtener el botón de filtro
+const botonDeFiltro = document.getElementById("clickMe");
+
+// Función que filtra los productos según el tipo o el color
+const filtrado = (productos = [], texto) => {
+  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+};
+
+// Agregar un evento de clic al botón de filtro
 botonDeFiltro.onclick = function() {
+  // Eliminar todos los productos de la lista
   while (li.firstChild) {
     li.removeChild(li.firstChild);
   }
 
-  const texto = $i.value;
+  // Obtener el texto de entrada
+  const texto = $i.value; 
   console.log(texto);
+
+  // Filtrar los productos según el texto de entrada
   const productosFiltrados = filtrado(productos, texto );
 
+  // Mostrar los productos filtrados
+  displayProductos(productosFiltrados);
+
+  //---------------
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
+    var d = document.createElement("div");
+    d.classList.add("producto");
   
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
+    var ti = document.createElement("p");
+    ti.classList.add("titulo");
+    ti.textContent = productosFiltrados[i].nombre;
     
     var imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
+    d.appendChild(ti);
+    d.appendChild(imagen);
   
-    li.appendChild(d)
+    li.appendChild(d);
   }
-}
+};
 
-const filtrado = (productos = [], texto) => {
+const filtro= (productos = [], texto) => {
   return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+
+};
